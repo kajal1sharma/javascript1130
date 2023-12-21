e601fcdc0c65abdc35d5fe26f8ec85b5ad3d8c93
kajal <kajal@gmail.com>
2023-12-21 12:56:51 +0530

    ..

--- script4.js
+++ script4.js
@@ -1,17 +1,61 @@
+// falsy values
+// "" undefined null NAN 0 -0 0n -0n false
+
+let value=90;
+switch(false){
+    case value<50:console.log("less than 50");
+                    break;
+    case value>50:console.log("more than 50");
+                    break;
+   
+    default:console.log("invalid")
+}
+
+
+
+// if(-1){
+//     console.log("result is true ")
+// }
+// else{
+//     console.log("result is false")
+// }
+
+// let value =90
+
+
+// if(value>50){
+//     console.log("discount")
+// }
+// else if(value>90){
+
+// }
+// else{
+
+// }
+
+
+
+// if(value>80){
+//     console.log("offer granted")
+// }
+// else{
+
+// }
+
 //calculator
 // + , - , *, /
 
-let num = Number(prompt("Enter some number"))
+// let num = Number(prompt("Enter some number"))
 
-switch(num % 5){
-    case 1:console.log("1jshjkdhs");
-            break;
-    case 2: console.log("2sadiijsadk");
-            break;
-    case 3:console.log("3sadijsdij")
-            break;
-    default:console.log("invalid")
-}
+// switch(num % 5){
+//     case 1:console.log("1jshjkdhs");
+//             break;
+//     case 2: console.log("2sadiijsadk");
+//             break;
+//     case 3:console.log("3sadijsdij")
+//             break;
+//     default:console.log("invalid")
+// }
 
 
 // let num1 = prompt("enter first number");
@@ -42,25 +86,25 @@ switch(num % 5){
 // }
 
 
-let result =0;
-if(symbol == "+"){
-    result = num1+num2;
-}
-else if( symbol == "-"){
-    result  = num1-num2;
-}
-else if(symbol == "*"){
-    result =num1*num2
-}
-else if(symbol=="/"){
-    result = num1/num2
-}
-else if( symbol =="**"){
-    result = num1**num2
-}
-else{
-    console.log("not a valid symbol")
-}
+// let result =0;
+// if(symbol == "+"){
+//     result = num1+num2;
+// }
+// else if( symbol == "-"){
+//     result  = num1-num2;
+// }
+// else if(symbol == "*"){
+//     result =num1*num2
+// }
+// else if(symbol=="/"){
+//     result = num1/num2
+// }
+// else if( symbol =="**"){
+//     result = num1**num2
+// }
+// else{
+//     console.log("not a valid symbol")
+// }
 
 
 // console.log(result)
