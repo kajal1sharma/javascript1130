9b510371a34ea8d9386a71f63c765aa8de05d959
kajal <kajal@gmail.com>
2024-01-01 11:58:46 +0530

    ..

--- script5.js
+++ script5.js
@@ -11,11 +11,18 @@
 
 
 
-for(let choice  ='1';choice=='1';choice = prompt("enter 1 to continue")){
+while(true){
+
         
+
         let ch =prompt("enter + ,  - , * , /");
         let num1 = Number(prompt("enter number one"))
         let num2 =Number(prompt("enter number two"))
+        if(num1 || num2){
+            alert("not a number")
+            continue;
+        }
+       
         switch(ch){
             case '+':alert(num1+num2);
                     break;
@@ -28,7 +35,32 @@ for(let choice  ='1';choice=='1';choice = prompt("enter 1 to continue")){
                     break;
             default : alert("Invalid choice")
         }
+        let choice =prompt("enter 1 to continue");
+        if(choice != '1'){
+            break;
+        }
 }
+
+alert("operation completed")
+
+// for(let choice  ='1';choice=='1';choice = prompt("enter 1 to continue")){
+        
+//         let ch =prompt("enter + ,  - , * , /");
+//         let num1 = Number(prompt("enter number one"))
+//         let num2 =Number(prompt("enter number two"))
+//         switch(ch){
+//             case '+':alert(num1+num2);
+//                     break;
+    
+//             case '-':alert(num1-num2);
+//                     break;
+//             case '*':alert(num1*num2);
+//                     break;
+//             case '/':alert(num1/num2);
+//                     break;
+//             default : alert("Invalid choice")
+//         }
+// }
 
 
 // let choice;
---
+++ script6.js
@@ -0,0 +1,36 @@
+console.log("arrays Topic")
+
+let arr=[1,2,3,4,5]
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+
+
+// collection of data 
+//in a single unit is 
+//called as array
+
+
+// let student1 ="reena";
+// let student2 ="tina";
+// let student3="ram";
+// // 60
+
+// let studentNames =["reena","tina","ram"];
+// studentNames[0] ="reena gupta";
+// studentNames[1] ="tina xyz";
+// studentNames[2] ="ram rst"
+
+
+// console.log(studentNames)
+
+
+// let arr = [1232.234,false,"iam a string", "kskdnfs",2334]
+
+// console.log(arr[0]);
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr)
