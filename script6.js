8bc1f00e6a9fb44d4933890a6f606600fd41abeb
kajal <kajal@gmail.com>
2024-01-01 12:54:27 +0530

    ..

--- script6.js
+++ script6.js
@@ -1,12 +1,72 @@
 console.log("arrays Topic")
 
-let arr=[1,2,3,4,5]
 
-console.log(arr[0])
-console.log(arr[1])
-console.log(arr[2])
-console.log(arr[3])
-console.log(arr[4])
+let arr = [12,13,14,15,64]
+let sum=0;
+// for(let  i =0 ;i < arr.length ;i++){
+//     sum =arr[i]+sum;
+// }
+
+for(let val of arr){
+    sum =sum+val;
+}
+
+console.log(sum)
+
+
+let obj ={name :"priya",rollno:24, phone:"234728374982374",marks:[45,67,78,98,78]};
+
+// let sum =0;
+// for(let i=0;i<obj.marks.length;i++){
+//     sum =sum +obj.marks[i];
+// }
+
+let total =0;
+for (let marks of obj.marks){
+    total =total +marks;
+}
+
+// console.log(obj.name + " has got "+total+" marks")
+
+// obj.name="priya verma"
+// obj["rollno"]=67;
+
+
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+
+// let arr =["rina","tina","ria"]
+
+// for(let key in arr){
+    
+//    console.log(key +"  "+arr[key]) 
+// }
+
+
+// console.log(obj)
+
+//let arr=["priya","anu","tina","reena"]
+
+// for -of 
+// for(let ele of arr){
+//     console.log(ele)
+// }
+
+// //iterator and indexer
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+
+
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
 
 
 // collection of data 
