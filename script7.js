61c7e89d8cb853c398175c213f17d92d63f1d4a3
kajal <kajal@gmail.com>
2024-01-03 11:39:06 +0530

    ..

---
+++ script7.js
@@ -0,0 +1,109 @@
+
+function wish(){
+    console.log("happy birthday")
+}
+
+for(let i=0;i<5;i++){
+    wish()
+}
+
+wish()
+//
+let a=90;
+console.log(a)
+wish()
+let b=80;
+let c=70
+console.log(b-c)
+wish()
+
+wish()
+
+//function definition
+// function sum(){
+//     let num1=90;
+//     let num2 =80;
+//     console.log(num1+num2);
+// }
+
+// sum();
+// console.log("hello world")
+// sum()
+// //task
+
+// sum()
+
+
+// let num1=90;
+// let num2 =80;
+// console.log(num1+num2);
+// logic
+
+// num1=190;
+// num2 =180;
+// console.log(num1+num2);
+
+
+//logic
+// num1=1920;
+// num2 =1820;
+// console.log(num1+num2);
+
+// let emp1 =9000
+// let emp2 =10000
+// let emp3 =5000
+
+// let sum = emp1 + emp2 +emp3;
+
+// let emp = [20000, 30000, 40000, 50000];
+
+// // let sum2 =emp[0]+emp[1]+emp[2]+emp[3];
+// let sum2=0;
+// for(let i=0 ; i< emp.length;i++){
+//     sum2 =sum2 + emp[i];
+// }
+// console.log(sum2)
+
+
+// given that the array going to be only with one level nesting
+// let arr=[20,"dsklfks",45,false,{}]
+
+// let sum=0;
+// for(let i=0;i<arr.length ; i++){
+   
+//     if(typeof arr[i] =="number"){
+//         sum=sum+arr[i];
+//     }
+    
+// }
+
+// console.log(sum)
+
+// let arr2 =[20,"dsklfks",45,false,[12,"dflklsdlkf",50],[10,20]]
+
+// let sum =0;
+// for(let i=0;i<arr2.length;i++){
+//     if(typeof arr2[i]=="number"){
+//         sum=sum+arr2[i];
+//     }
+//     else if(typeof arr2[i]=="object"){
+        
+//         for(let num of arr2[i]){
+//             if(typeof num=="number"){
+//                 sum=sum+num;
+//             }
+//         }
+
+//     }
+// }
+
+// console.log(sum)
+
+
+// let arr =[12,"this is a string ",[1,2,3,4],{name:"tina"},null, undefined]
+
+// console.log(arr)
+
+
+// obj ={1:"dfhaj",2:"jlkfsfdhkasdj",3:"dsjfhnsjkdf"}
+// console.log(obj)
