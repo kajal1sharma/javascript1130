803ce386cd3dd63c1a3c919e11e23d3a0ed491b4
kajal <kajal@gmail.com>
2024-01-11 12:30:39 +0530

    ..

---
+++ script13.js
@@ -0,0 +1,74 @@
+// function expresssion
+
+let print = function(){
+                console.log("hello world")
+            }
+
+            console.log(print)
+print();
+
+let greeting = function abc(){
+                    console.log("happy birthday")
+                }
+console.log(greeting)
+greeting()
+
+// function declaration
+
+
+// console.log("script 13")
+
+
+// function decalaration
+function sum(a,b){
+    a=a+10;
+    console.log(a);
+    let sum =a+b
+}
+
+
+// let p =90;
+// let q =100
+// sum(q,p)
+// console.log(q)
+
+// function sum(){
+//     console.log("hello world")
+// }
+
+// console.log(sum)
+
+// function sum(arr){
+
+//     arr[0]=90;
+//     arr[1]=80;
+//     arr[2]=60;
+//     console.log(arr)
+
+// }
+
+// let brr=[1,2,3,4,5,6];
+// sum(brr)
+// console.log(brr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
