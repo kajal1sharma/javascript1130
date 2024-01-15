d266f5a3f3b6372bcd55dd60c738623c8bfd31c4
kajal <kajal@gmail.com>
2024-01-15 12:39:18 +0530

    ..

--- script14.js
+++ script14.js
@@ -1,6 +1,34 @@
 console.log("script 14")
 
 
+function getName(){
+    console.log(this)
+}
+getName()
+phone="88888"
+name="nitu"
+
+let obj = {
+    name:"tina",
+    phone:"923492374",
+    getInfo:()=>{
+                console.log(this)
+                console.log(this.name)
+                console.log(this.phone)
+            }
+}
+
+obj.getInfo()
+
+// console.log(this)
+// function print(){
+//     console.log(this)
+// }
+
+// print()
+
+
+
 // function read(){
 //     console.log("i read something")
 // }
@@ -48,20 +76,21 @@ console.log("script 14")
 
 //  getWish("tina")
 
-let add = (a,b)=>a+b
+// let add = (a,b)=>a+b
 
-let sum1 = add(12,13);
-console.log(sum1)
+// let sum1 = add(12,13);
+// console.log(sum1)
 
-let add2 = (a,b)=>{
-    return a+b
-}
-let sum2 = add2(12,13);
-console.log(sum2)
+// let add2 = (a,b)=>{
+//     return a+b
+// }
+// let sum2 = add2(12,13);
+// console.log(sum2)
 
-let wish = name=>"happy birthday "+name
-let str = wish("tina")
-console.log(str)
+// let wish = name=>"happy birthday "+name
+// let str = wish("tina")
+// console.log(str)
+
 
 
 
