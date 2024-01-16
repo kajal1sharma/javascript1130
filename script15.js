1c8a15df3af95b7cbe4010075df631b9ac08630c
kajal <kajal@gmail.com>
2024-01-16 12:35:22 +0530

    ..

--- script15.js
+++ script15.js
@@ -1,41 +1,60 @@
-// abc (apply bind and call)
+// // abc (apply bind and call)
 
-// a=> apply func
-// b=> bind func
-//  c=> call func
+// // a=> apply func
+// // b=> bind func
+// //  c=> call func
 
 
-let student1 ={
-    name:"reena",
-    roll:45,
-    fees:234234,
-    marks:344
-}
+// let student1 ={
+//     name:"reena",
+//     roll:45,
+//     fees:234234,
+//     marks:344
+// }
 
-let student2 ={
-    name:"tina",
-    roll:42,
-    fees:234234,
-    marks:341
+// let student2 ={
+//     name:"tina",
+//     roll:42,
+//     fees:234234,
+//     marks:341
     
-}
+// }
 
-let getInfo = function(a,b,c,d){
-                    console.log(a,b,c)
-                    console.log(this)
-                    console.log(this.name+"--"+this.roll+"--"+this.fees+"--"+this.marks );
-                }
-// getInfo() 
+// let getInfo = function(a,b,c,d){
+//                     console.log(a,b,c,d)
+//                     console.log(this)
+//                     console.log(this.name+"--"+this.roll+"--"+this.fees+"--"+this.marks );
+//                 }
+// // getInfo() 
 
-getInfo([1,2,3,4],"hindi", "english","maths")
+// getInfo([1,2,3,4],"hindi", "english","maths")
 
-getInfo.call(student1,[1,2,3,4],"hindi", "english","maths")
-getInfo.apply(student2,[[1,2,34],"hindi1", "english1","maths1"])
+// getInfo.call(student1,[1,2,3,4],"hindi", "english","maths")
+// getInfo.apply(student2,[[1,2,34],"hindi1", "english1","maths1"])
 
 // let getInfoSt1 = getInfo.bind(student1)
 // let getInfoSt2 = getInfo.bind(student2)
 // getInfoSt1()
 // getInfoSt2()
+
+
+function doSomething(fn){
+    console.log("hello world")
+    //console.log(fn)
+     fn();
+}
+function eat(){
+    console.log("lets eat somethoing")
+    return 10
+}
+function read(){
+    console.log("i read a lot of books")
+}
+doSomething(eat)
+doSomething(read)
+
+
+
 
 
 
