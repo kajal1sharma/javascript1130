15015c143c60ffe284fd0a94c1e1e3920b4361c8
kajal <kajal@gmail.com>
2024-01-24 12:14:24 +0530

    ..

--- script19.js
+++ script19.js
@@ -2,29 +2,45 @@ console.log("hello world")
 
 
 let arr= [1,2,3,4,5]
-console.log(arr)
 
-let obj ={
-    0:1,
-    1:2,
-    2:32,
-    3:4,
-    4:56,
-    5:34,
-    length:6,
-
-    forEach:function(fn){
-            for(let i=0;i<this.length;i++){
-                let ele= this[i]
-                fn(ele,i)
-            }           
-    }
-}
+arr.concat
+arr.every 
+arr.fill
+arr.map
+arr.find
+arr.findIndex
+arr.includes
+arr.toString
+arr.indexOf
+arr.reverse
+arr.sort
+arr.join
 
 
-obj.forEach(function(ele, i){
-    console.log(ele+"++"+i)
-})
+
+// console.log(arr)
+
+// let obj ={
+//     0:1,
+//     1:2,
+//     2:32,
+//     3:4,
+//     4:56,
+//     5:34,
+//     length:6,
+
+//     forEach:function(fn){
+//             for(let i=0;i<this.length;i++){
+//                 let ele= this[i]
+//                 fn(ele,i)
+//             }           
+//     }
+// }
+// obj.forEach(function(ele, i){
+//     console.log(ele+"++"+i)
+// })
+
+
 
 
 
