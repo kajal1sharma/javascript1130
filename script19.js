a8f02ace57a390f42d9db08b73a7740b37bbbbb3
kajal <kajal@gmail.com>
2024-01-25 12:44:59 +0530

    ..

--- script19.js
+++ script19.js
@@ -42,28 +42,65 @@ console.log("hello world")
 // console.log(arr)//in-place
 
 
-let arr =["ram","sita","rita","geeta","tia"]
-let res= arr.map((ele)=>{
-    if(ele.length>3){
-        return "happy birthday "+ele
-    }
-    else{
-        return "good morning "+ele
-    }
-    // return "hello "+ele
-})
-console.log(res)
-console.log(arr)
+// let arr =["ram","sita","rita","geeta","tia"]
+// let res= arr.map((ele)=>{
+//     if(ele.length>3){
+//         return "happy birthday "+ele
+//     }
+//     else{
+//         return "good morning "+ele
+//     }
+//     // return "hello "+ele
+// })
+// console.log(res)
+// console.log(arr)
+
+// let arr=[1,9,4,0,5,7,8]
 
 
-// arr.find
+// let res=arr.findIndex((ele, index, arr)=>{
+//         if(ele%2==0)
+//         return true
+//     })
+//     console.log(res)
+
+// let result =arr.find((ele, index, arr)=>{
+//     if(ele%2==0)
+//     return true
+// })
+// console.log(result)
+
+
 // arr.findIndex
-// arr.includes
-// arr.toString
-// arr.indexOf
-// arr.reverse
-// arr.sort
-// arr.join
+
+// let arr =[1,2,3,4,23,5,1,6,7,7]
+// let result =arr.includes(1,4)
+// console.log(result)
+
+
+// let arr=[1,2,3,4,5,5,5,[1,1,1,[0,0,0]]] //nested
+// let res =  arr.toString()
+// console.log(res)
+
+
+
+
+//Q arr.indexOf and findIndex
+
+
+ //let res= arr.reverse();
+//  console.log(res)
+//  console.log(arr)
+//  in place
+
+//arr.sort
+
+// let arr =[1,5,6,7,2,5]
+// let result = arr.join(",");
+// console.log(typeof result)
+// console.log(result)
+
+
 
 
 
