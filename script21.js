eea3b87d37527f5963a390be59c2d88eee09ec67
kajal <kajal@gmail.com>
2024-01-31 12:09:29 +0530

    ..

---
+++ script21.js
@@ -0,0 +1,49 @@
+// hoisting
+
+// console.log(num)
+
+
+//functional scope
+// var => fnctional scope
+//let or const  => block
+function print(){
+
+// hoisting scope(block, functional)
+    {
+        var a=90;
+        console.log(b)
+        let b =80;
+        // console.log(b)
+    }
+    // console.log(a)
+    // console.log(b)
+    // console.log(num)
+    // var num =0;
+    // console.log(a)
+    // console.log(num)
+    // //block scope
+    // {
+    //     console.log(num)
+    //     var num=10;
+    // }
+
+    // console.log(num)
+
+    // var keyword => functional scope
+}
+print()
+
+
+// var, let , const 
+
+// console.log(num)
+// var num =90;
+
+// // console.log(num2)  //temporal dead zone (variable is not visible)
+
+// let num2 ;
+// console.log(num2)
+
+
+// const num3 =90;
+// console.log(num3)
