cec25b466fc05e181dc82f561f74ae501a7dc97d
kajal <kajal@gmail.com>
2024-02-06 11:51:41 +0530

    ..

---
+++ script23.js
@@ -0,0 +1,42 @@
+// console.log("helllo world")
+
+// let str = "this is my first string"
+// let str2 = 'this is my second string'
+
+// console.log(str)
+// console.log(str2)
+
+
+// let name ="tina"
+// let age =89;
+
+// // let wish = "happy birthday " + name +" on your "+age+"th birthday"
+
+// //template string
+
+// let wish = `happy birthday ${name} on your ${age}th birthday`
+
+// console.log(wish)
+
+
+
+let str2 =`this is my first 
+this is my second line`;
+console.log(str2)
+
+ let str= "this is first line \n kdhfdfjkshfjdj"
+console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
