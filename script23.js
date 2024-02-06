48d48b959667ad4654ac2c2a81d4069a4b2e8b11
kajal <kajal@gmail.com>
2024-02-06 12:18:40 +0530

    ..

--- script23.js
+++ script23.js
@@ -1,3 +1,34 @@
+
+let str= "         first is a place is    ";
+console.log(typeof str)
+
+// console.log(str.length)
+// console.log(str.charAt(2))
+// console.log(str.startsWith("is",6))
+// console.log(str.endsWith("st"))
+// console.log(str.substring(3,6))
+// console.log(str.slice(4 ,8))
+// console.log(str.charCodeAt(3))
+// console.log(str.replace("is","this"))
+// console.log(str.replaceAll("is","this"))
+// let strarr = str.split(" ")
+// // regex => make 
+
+// console.log(strarr)
+// console.log(str.trim())
+// console.log(str.trimEnd())
+// console.log(str.trimStart())
+
+
+
+
+
+
+
+
+
+
+
 // console.log("helllo world")
 
 // let str = "this is my first string"
@@ -20,12 +51,12 @@
 
 
 
-let str2 =`this is my first 
-this is my second line`;
-console.log(str2)
+// let str2 =`this is my first 
+// this is my second line`;
+// console.log(str2)
 
- let str= "this is first line \n kdhfdfjkshfjdj"
-console.log(str)
+//  let str= "this is first line \n kdhfdfjkshfjdj"
+// console.log(str)
 
 
 
