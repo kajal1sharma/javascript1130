c944e412e976de6dfc3790d327277684f86f7169
kajal <kajal@gmail.com>
2024-02-14 12:17:34 +0530

    ..

---
+++ script25.js
@@ -0,0 +1,34 @@
+
+let id= setTimeout(()=>{
+    console.log("hello world")
+},10000)
+
+
+let a = prompt("get value");
+if(a==10){
+    clearTimeout(id)
+}
+
+
+
+
+
+// console.log("script 25");
+
+
+// let a=90;
+
+// let setID= setTimeout(()=>{
+
+//     // console.log(a+10);
+// // return 10;
+// },5000)
+
+// let setID2= setTimeout(()=>{
+
+//     // console.log(a+10);
+// // return 10;
+// },5000)
+
+//  console.log(setID)
+//  console.log(setID2)
