25cdc128a0970d56829b1267719eb9d978174740
kajal <kajal@gmail.com>
2024-02-19 12:08:08 +0530

    ..

--- script26.js
+++ script26.js
@@ -1,30 +1,45 @@
 
+
+
+fetch("https://dummyjson.com/todos")
+.then(function(data){
+    return data.json()
+})
+.then(function(data){
+    console.log(data)
+})
+.catch(function(err){
+    console.log("some error occurred")
+})
+
+
+
 //promises
 
 //promise => //pending
-//fulfilled 
-//rejected
+// //fulfilled 
+// //rejected
 
 
-let promise  =  new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        let a=5;
-        if(a>5){
-            reject("this function has failed")
-        }
-        else{
-            resolve("this function has resolved")
-        }
-    }, 5000);
-})
+// let promise  =  new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         let a=5;
+//         if(a>5){
+//             reject("this function has failed")
+//         }
+//         else{
+//             resolve("this function has resolved")
+//         }
+//     }, 5000);
+// })
 // console.log(promise)
 
-promise.then((data)=>console.log(data))
-.catch((err)=>{console.log(err)})
+// promise.then((data)=>console.log(data))
+// .catch((err)=>{console.log(err)})
 
 // console.log(promise,"+++++++++++")
 
-
+// pending , 
 
 
 //iife => immediately invoked function ecpression
