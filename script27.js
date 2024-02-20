ce37b8a8ab5e9957ab589095c6e3a197777ca307
kajal <kajal@gmail.com>
2024-02-20 11:52:20 +0530

    ..

---
+++ script27.js
@@ -0,0 +1,11 @@
+
+
+
+window.addEventListener('load',function(){
+    console.log("page has been loaded")
+
+
+    let buttonvar = this.document.getElementById("buttonpress")
+    console.log(buttonvar)
+
+})
