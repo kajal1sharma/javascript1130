a451b29f77e56a5551c0b90407d86f8cfb7340e4
kajal <kajal@gmail.com>
2024-02-21 12:49:08 +0530

    ..

--- script28.js
+++ script28.js
@@ -1,16 +1,48 @@
 
-window.addEventListener('load',()=>{
-    console.log("document has been loaded")
-    let buttonele=  document.getElementById("button");
+ window.addEventListener('load',()=>{
     let paraele = document.getElementById("para")
-    console.log(buttonele)
-    console.log(paraele)
-    let count =0 
+    let startButton =document.getElementById("buttonStart");
+    let stopButton =document.getElementById("buttonStop");
+    let resetButton =document.getElementById("buttonReset");
 
-    buttonele.addEventListener('click',()=>{
-        count++;
-        paraele.innerText=count;
+    let intervalid=null;
+    let count=0;
+    startButton.addEventListener('click',()=>{
+        intervalid = setInterval(()=>{
+            count++;
+            paraele.innerText =count;
+        },1000)
     })
 
 
-})
+    stopButton.addEventListener('click',()=>{
+
+        clearInterval(intervalid)
+    })
+
+    resetButton.addEventListener('click',()=>{
+
+        clearInterval(intervalid)
+        count=0;
+        paraele.innerText =count;
+
+    })
+
+ })
+
+
+// window.addEventListener('load',()=>{
+//     console.log("document has been loaded")
+//     let buttonele=  document.getElementById("button");
+//     let paraele = document.getElementById("para")
+//     console.log(buttonele)
+//     console.log(paraele)
+//     let count =0 
+
+//     buttonele.addEventListener('click',()=>{
+//         count++;
+//         paraele.innerText=count;
+//     })
+
+
+// })
