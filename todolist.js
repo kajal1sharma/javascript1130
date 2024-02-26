d5ce38b3e41b14a799b83362abbdbf587eb90190
kajal <kajal@gmail.com>
2024-02-26 12:31:03 +0530

    ..

--- todolist.js
+++ todolist.js
@@ -1,10 +1,19 @@
 window.addEventListener('load',function(){
 
+
     let addButton = document.getElementById('inputbutton');
     let inputField = this.document.getElementById("inputfield");
+    let task="";
+    let taskcontainer =this.document.getElementById('taskdisplayer')
 
     inputField.addEventListener('input',function(event){
         // console.log("input")
-        console.log(event.target.value)
+        task= event.target.value
+    })
+
+    addButton.addEventListener('click',function(){
+       let para=  document.createElement('p')
+       para.innerText= task;
+       taskcontainer.appendChild(para)
     })
 })
